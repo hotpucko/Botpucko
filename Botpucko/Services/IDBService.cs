f8b3784266577f49f40a22b2ce85ea56e5a32150
Hotpucko <stn.jonsson@gmail.com>
2022-07-11 12:08:39 +0200

    Added a database service interface

---
+++ Botpucko/Services/IDBService.cs
@@ -0,0 +1,23 @@
+﻿using Botpucko.Models;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Botpucko.Services
+{
+    internal interface IDBService<T> where T : class
+    {
+        public static abstract Task<T> CreateAsync(IConfiguration Configuration);
+
+        public Task AddGuildItemAsync(Guild guild);
+
+        public Task<Guild> GetGuildItemAsync(int id);
+
+        public Task ReplaceGuildDateItemAsync(int id, SessionDate date);
+
+        public Task DeleteGuildItemAsync(int id);
+    }
+}
