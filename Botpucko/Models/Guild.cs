8d352cfc77bf6091b79e82e9df9899f6d696e0b8
Hotpucko <stn.jonsson@gmail.com>
2022-07-11 12:05:08 +0200

    Added data model

---
+++ Botpucko/Models/Guild.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Botpucko.Models
+{
+    internal class Guild
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        public SessionDate Date { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+
+    }
+
+    internal class SessionDate
+    {
+        public DayOfWeek Day { get; set; }
+        public TimeOnly Time { get; set; }
+    }
+}
