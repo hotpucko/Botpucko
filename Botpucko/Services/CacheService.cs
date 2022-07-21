9d28c11de3df8f35fdeec18fce4598e8e0969771
Hotpucko <stn.jonsson@gmail.com>
2022-07-21 21:30:56 +0200

    Added a client side cache for data

---
+++ Botpucko/Services/CacheService.cs
@@ -0,0 +1,30 @@
+﻿using Botpucko.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Botpucko.Services
+{
+    public class CacheService
+    {
+        private readonly Dictionary<string, Guild> Guilds;
+
+        public CacheService()
+        {
+            Guilds = new Dictionary<string, Guild>();
+        }
+
+        public Guild? GetGuildIfExists(string key)
+        {
+            Guilds.TryGetValue(key, out Guild? guild);
+            return guild;
+        }
+
+        public void InsertGuild(string key, Guild guild)
+        {
+            Guilds.TryAdd(key, guild);
+        }
+    }
+}
