80a3538ebd094f5d60bff97071f22645d000c042
Hotpucko <stn.jonsson@gmail.com>
2022-07-21 21:25:15 +0200

    Refactored to use new error messages and removed magic numbers

--- Botpucko/Modules/RollModule.cs
+++ Botpucko/Modules/RollModule.cs
@@ -13,29 +13,38 @@ namespace Botpucko.Modules
         [Summary("Rolls x dice of y type")]
         public Task RollAsync(string str)
         {
+            const int maxDieCount = 1000;
+            const int maxDieSides = 10000;
+
             str = str.ToLower();
             if (!str.Contains('d'))
             {
-                ReplyAsync(Utility.StaticErrorMessages.RollError);
+                ReplyAsync(Utility.ErrorMessages.RollModule.SyntaxError);
                 return Task.CompletedTask;
             }
 
             string[] strings = str.Split('d');
             if (strings.Length != 2)
             {
-                ReplyAsync(Utility.StaticErrorMessages.RollError);
+                ReplyAsync(Utility.ErrorMessages.RollModule.SyntaxError);
                 return Task.CompletedTask;
             }
 
             if (!int.TryParse(strings[0], out int x) || !int.TryParse(strings[1], out int y))
             {
-                ReplyAsync(Utility.StaticErrorMessages.RollError);
+                ReplyAsync(Utility.ErrorMessages.RollModule.SyntaxError);
                 return Task.CompletedTask;
             }
 
             if (x <= 0 || y <= 0)
             {
-                ReplyAsync(Utility.StaticErrorMessages.RollError);
+                ReplyAsync(Utility.ErrorMessages.RollModule.SyntaxError);
+                return Task.CompletedTask;
+            }
+
+            if (x > maxDieCount || y > maxDieSides)
+            {
+                ReplyAsync(String.Format(Utility.ErrorMessages.RollModule.SizeError));
                 return Task.CompletedTask;
             }
 
